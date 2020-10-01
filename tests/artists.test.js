381d3e9600858412a3b5c5544714ab517e9e8e86
Sam Tovey <samtovey13@gmail.com>
2020-10-01 21:22:05 +0100

    set up test suite to run with first test (successfully) failing

---
+++ tests/artists.test.js
@@ -0,0 +1,39 @@
+/* artists.test.js */
+const { expect } = require("chai");
+const request = require("supertest");
+const { Artist } = require("../src/models");
+const app = require("../src/app");
+
+describe("/artists", () => {
+  // before in mocha is like beforeAll in jest. //
+  // we are using it to connect to the Artist table //
+  before(async () => {
+    try {
+      await Artist.sequelize.sync();
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  // to clean out the Artist table before each test //
+  beforeEach(async () => {
+    try {
+      await Artist.destroy({ where: {} });
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  //test creates a new artist by sending the data (name + genre) to an /artists endpoint //
+
+  describe("POST /artists", async () => {
+    it("creates a new artist in the database", async () => {
+      const response = await request(app).post("/artists").send({
+        name: "Tame Impala",
+        genre: "Rock",
+      });
+
+      await expect(response.status).to.equal(201);
+    });
+  });
+});
