7c186996714b83b5c649d486fe07c9d68f76d928
Sam Tovey <samtovey13@gmail.com>
2020-10-01 22:10:17 +0100

    added tests for adding a new artist to the database, wrote code to pass the tests

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@ const express = require("express");
 
 const app = express();
 
+const artistControllers = require("./controllers/artists"); //controller function imported
+
 app.use(express.json()); //middleware
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello World");
 });
+
+app.post("/artists", artistControllers.create);
 
 module.exports = app;
