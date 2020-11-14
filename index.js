1527b2caa1a6f7eaf6f0da38d6c5b3aed2f18491
Sam Tovey <samtovey13@gmail.com>
2020-11-14 16:05:24 +0000

    added code for deploying to heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./src/app');
 
-const APP_PORT = 4000;
+const APP_PORT = process.env.port || 4000;
 
 app.listen(APP_PORT, () => {
   console.log(`Now serving your Express app at http://localhost:${APP_PORT}`); //eslint-disable-line
--- src/models/index.js
+++ src/models/index.js
@@ -6,7 +6,9 @@ const SongModel = require("./song");
 const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
 
 const setupDatabase = () => {
-  const connection = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  const connection = CLEARDB_DATABASE_URL ?
+  new Sequelize(CLEARDB_DATABASE_URL) :
+  new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
     port: DB_PORT,
     dialect: 'mysql',
